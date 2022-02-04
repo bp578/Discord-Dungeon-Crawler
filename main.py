6df3e60bbf65ce5216b40f4791e3fb542a5ff722
MadScientist2854 <madscientist2854@gmail.com>
2022-02-04 18:35:17 +0500

    implemented \!help command

--- main.py
+++ main.py
@@ -4,25 +4,35 @@ import discord
 
 bot = commands.Bot(
 	command_prefix="!",
-	case_insensitive=True
+	case_insensitive=True,
+	help_command=None
 )
 
 bot.author_id = 487258918465306634  # Change to your discord id!!!
 
+@bot.event
 async def on_ready():  # When the bot is ready
-    print("I'm in")
-    print(bot.user)  # Prints the bot's username and identifier
+	print("I'm in")
+	print(bot.user)  # Prints the bot's username and identifier
 
+@bot.command()
+async def ping(ctx):
+    await ctx.reply("pong!")
 @bot.command()
 async def repeat(ctx, arg):
 	await ctx.send(arg)
 @bot.command()
-async def help2(ctx):
-	await ctx.send("I am unhelpful")
+async def help(ctx):
+	help_msg = """
+	ng: Begin a new game
+eg: End current game
+stats: Display your current stats
+	"""
+	await ctx.send(help_msg)
 @bot.command()
 async def message(ctx, message="Hello"):
-  user = ctx.author
-  await user.send(message)
+	user = ctx.author
+	await user.send(message)
 
 token = "OTM4OTY5NDk3MTI0MDg1ODQx.YfyBfQ.smaJcZWXYUp8J5Dv82yH9sH2CUU"
 bot.run(token)  # Starts the bot
