a838d64df7ff254fa965886a5b13aeb38d134c11
Ben Phung <bp578@drexel.edu>
2022-02-15 02:12:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -29,5 +29,21 @@ stats: Display your current stats"""
   user = ctx.author
   await user.send(text)
 
+@bot.command()
+async def ng(ctx):
+  await ctx.reply("A new adventure begins in a dark dungeon...")
+
+@bot.command()
+async def eg(ctx):
+  await ctx.reply("You have cut your own life short. Better luck on your next adventure.")
+
+@bot.command()
+async def stats(ctx):
+  text = """ HP: 100  
+ATK: 10
+DEF: 5
+SPD: 5"""
+  await ctx.reply(text)
+
 token = "OTM4OTY5NDk3MTI0MDg1ODQx.YfyBfQ.smaJcZWXYUp8J5Dv82yH9sH2CUU"
 bot.run(token)  # Starts the bot
