5b0c0479176bdf51e4c4fa2cdb5f0a72bd4c78c5
Ben Phung <bp578@drexel.edu>
2022-04-29 21:15:48 +0000

    Upload New File

---
+++ enemyImages.py
@@ -0,0 +1,7 @@
+#This file stores links to each enemy image as a string
+dungeonImage = 'https://i.imgur.com/Ydzpnhs.jpg'
+goblinImage = 'https://i.imgur.com/NkJ9Gjz.jpg'
+ogreImage = 'https://i.imgur.com/6fTUU3Z.jpg'
+ratImage = 'https://i.imgur.com/vF3AK4F.jpg'
+spiderImage = 'https://i.imgur.com/qzoLv7G.jpg'
+skeletonImage = 'https://i.imgur.com/w3gdYwR.jpg'
